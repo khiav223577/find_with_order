2d2f4e731b3c9b11f5bc247982d89e2a70094e47
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-02-22 21:47:49 +0800

    add none for rails 3

--- lib/find_with_order.rb
+++ lib/find_with_order.rb
@@ -8,3 +8,10 @@ class << ActiveRecord::Base
     where(id: ids).order("field(id, #{ids.join(',')})")
   end
 end
+unless ActiveRecord::Base.respond_to?(:none) # extend only if not implement yet
+  class ActiveRecord::Base
+    def self.none #For Rails 3
+      where('1=0')
+    end
+  end
+end
