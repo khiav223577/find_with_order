cccf0be80b64f9415df7c87074892378f0b54cbe
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-12 01:30:14 +0800

    escape quote

--- lib/find_with_order/pg_support.rb
+++ lib/find_with_order/pg_support.rb
@@ -10,7 +10,7 @@ module FindWithOrder
       relation = relation.where(column => ids)
       case ids.first
       when Numeric ; return relation.order("array_position(ARRAY[#{ids.join(',')}], #{column})")
-      when String ; return relation.order("array_position(ARRAY['#{ids.map{|s| s.gsub("'", "\\'") }.join('\',\'')}']::varchar[], #{column})")
+      when String  ; return relation.order("array_position(ARRAY['#{ids.map{|s| s.gsub("'", "''") }.join('\',\'')}']::varchar[], #{column})")
       else
         raise 'not support type: #{ids.first.class}'
       end
