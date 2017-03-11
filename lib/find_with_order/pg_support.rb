feba54083096ef6f284d4433e0cca420443d111c
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-03-12 01:18:31 +0800

    support PG

--- lib/find_with_order/pg_support.rb
+++ lib/find_with_order/pg_support.rb
@@ -3,11 +3,17 @@ module FindWithOrder
   module PGSupport
     def self.find_with_order(relation, ids)
       ids = ids.uniq
-      return relation.where(id: ids).order("field(#{relation.table_name}.id, #{ids.join(',')})").to_a
+      return relation.where(id: ids).order("array_position(ARRAY[#{ids.join(',')}], #{relation.table_name}.id)").to_a
     end
     def self.where_with_order(relation, column, ids)
       ids = ids.uniq
-      return relation.where(column => ids).order("field(#{column}, #{ids.map(&:inspect).join(',')})")
+      relation = relation.where(column => ids)
+      case ids.first
+      when Numeric ; return relation.order("array_position(ARRAY[#{ids.join(',')}], #{column})")
+      when String ; return relation.order("array_position(ARRAY['#{ids.map{|s| s.gsub("'", "\\'") }.join('\',\'')}']::varchar[], #{column})")
+      else
+        raise 'not support type: #{ids.first.class}'
+      end
     end
   end
 end
