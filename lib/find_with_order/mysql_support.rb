ef8c44224f05aba071a873208112ea3981e75497
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2017-07-02 01:23:59 +0800

    implement PG version

--- lib/find_with_order/mysql_support.rb
+++ lib/find_with_order/mysql_support.rb
@@ -1,17 +1,15 @@
-module FindWithOrder
-  module MysqlSupport
-    class << self
-      def find_with_order(relation, ids)
-        return relation.where(id: ids).order("field(#{relation.table_name}.id, #{ids.join(',')})").to_a
-      end
+module FindWithOrder::MysqlSupport
+  class << self
+    def find_with_order(relation, ids)
+      return relation.where(id: ids).order("field(#{relation.table_name}.id, #{ids.join(',')})").to_a
+    end
 
-      def where_with_order(relation, column, ids)
-        return relation.where(column => ids).order("field(#{column}, #{ids.map(&:inspect).join(',')})")
-      end
+    def where_with_order(relation, column, ids)
+      return relation.where(column => ids).order("field(#{column}, #{ids.map(&:inspect).join(',')})")
+    end
 
-      def with_order(relation, column, ids)
-        relation.order("field(#{column}, #{ids.map(&:inspect).join(',')})")
-      end
+    def with_order(relation, column, ids)
+      relation.order("field(#{column}, #{ids.map(&:inspect).join(',')})")
     end
   end
 end
--- lib/find_with_order/pg_support.rb
+++ lib/find_with_order/pg_support.rb
@@ -1,13 +1,30 @@
-module FindWithOrder
-  module PGSupport
-    def self.find_with_order(relation, ids)
+module FindWithOrder::PGSupport
+  class << self
+    def find_with_order(relation, ids)
       # return relation.where(id: ids).order("array_position(ARRAY[#{ids.join(',')}], #{relation.table_name}.id)").to_a #array_position is only support in PG >= 9.5
       return relation.where(id: ids)
                      .joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{ids.join('),(')})) AS id(val)) AS id ON (#{relation.table_name}.id = id.val)")
                      .order('row_number')
     end
-    def self.where_with_order(relation, column, ids)
+
+    def where_with_order(relation, column, ids)
       relation = relation.where(column => ids)
+      case ids.first
+      when Numeric
+        # return relation.order("array_position(ARRAY[#{ids.join(',')}], #{column})") #array_position is only support in PG >= 9.5
+        return relation.joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{ids.join('),(')})) AS id(val)) AS id ON (#{column} = id.val)")
+                       .order('row_number')
+      when String
+        ids.map!{|s| ActiveRecord::Base.connection.quote_string(s) }
+        # return relation.order("array_position(ARRAY['#{ids.join("','")}']::varchar[], #{column})") #array_position is only support in PG >= 9.5
+        return relation.joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES('#{ids.join("'),('")}')) AS id(val)) AS id ON (#{column} = id.val)")
+                       .order('row_number')
+      else
+        raise "not support type: #{ids.first.class}"
+      end
+    end
+
+    def with_order(relation, column, ids)
       case ids.first
       when Numeric
         # return relation.order("array_position(ARRAY[#{ids.join(',')}], #{column})") #array_position is only support in PG >= 9.5
