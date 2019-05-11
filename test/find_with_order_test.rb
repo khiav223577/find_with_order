e23bdfb4ad1d1e34aed20c52e45dee37b1e38098
Rumble Huang <mrtmrt15xn@yahoo.com.tw>
2019-05-12 02:39:43 +0800

    add more test cases

--- test/find_with_order_test.rb
+++ test/find_with_order_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class FindWithOrderTest < Minitest::Test
   def setup
-    
   end
+
   def test_that_it_has_a_version_number
     refute_nil ::FindWithOrder::VERSION
   end
--- test/order_uuid_test.rb
+++ test/order_uuid_test.rb
@@ -10,7 +10,7 @@ class OrderUuidTest < Minitest::Test
 
   def test_find_id_with_order
     test_order = proc{|order|
-      expected = UuidUser.where(:id => order).to_a.sort_by{|user| order.index(user.id) }
+      expected = UuidUser.where(id: order).to_a.sort_by{|user| order.index(user.id) }
       assert_equal expected, UuidUser.find_with_order(order)
     }
     test_order.call [@user1.id, @user2.id, @user3.id]
@@ -19,6 +19,33 @@ class OrderUuidTest < Minitest::Test
     test_order.call [@user2.id, @user3.id, @user1.id]
     test_order.call [@user3.id, @user1.id, @user2.id]
     test_order.call [@user3.id, @user2.id, @user1.id]
+  end
+
+  def test_find_account_with_order
+    test_order = proc{|order|
+      expected = UuidUser.where(account: order).to_a.sort_by{|user| order.index(user.account) }
+      assert_equal expected, UuidUser.where_with_order(:account, order)
+    }
+    test_order.call %w(john jimmy peter)
+    test_order.call %w(john peter jimmy)
+    test_order.call %w(jimmy john peter)
+    test_order.call %w(jimmy peter john)
+    test_order.call %w(peter john jimmy)
+    test_order.call %w(peter jimmy john)
+  end
+
+  def test_none
+    assert_equal [], UuidUser.none.find_with_order([@user1.id, @user2.id, @user3.id])
+  end
+
+  def test_find_empty
+    assert_equal [], UuidUser.find_with_order([])
+    assert_equal [], UuidUser.find_with_order(nil)
+  end
+
+  def test_where_empty
+    assert_equal [], UuidUser.where_with_order(:account, []).to_a
+    assert_equal [], UuidUser.where_with_order(:account, nil).to_a
   end
 
   def test_with_order
