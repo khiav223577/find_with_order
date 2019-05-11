040683910f524793a7d8e98742ee23836ffb275e
Rumble Huang <mrtmrt15xn@yahoo.com.tw>
2019-05-12 02:33:28 +0800

    type cast uuid

--- lib/find_with_order/pg_support.rb
+++ lib/find_with_order/pg_support.rb
@@ -2,8 +2,11 @@ module FindWithOrder::PGSupport
   class << self
     def find_with_order(relation, ids)
       # return relation.where(id: ids).order("array_position(ARRAY[#{ids.join(',')}], #{relation.table_name}.id)").to_a #array_position is only support in PG >= 9.5
+      column_type = relation.columns_hash['id']&.type
+      id_val = column_type == :uuid ? '(id.val)::uuid' : 'id.val'
+      values = to_sql_values(ids)
       return relation.where(id: ids)
-                     .joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{to_sql_values(ids)})) AS id(val)) AS id ON (#{relation.table_name}.id = id.val)")
+                     .joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{values})) AS id(val)) AS id ON (#{relation.table_name}.id = #{id_val})")
                      .order('row_number')
     end
 
@@ -12,13 +15,16 @@ module FindWithOrder::PGSupport
     end
 
     def with_order(relation, column, ids, null_first: false)
+      column_type = relation.columns_hash[column.to_s]&.type
       if column.is_a?(Symbol) and relation.column_names.include?(column.to_s)
         column = "#{relation.connection.quote_table_name(relation.table_name)}.#{relation.connection.quote_column_name(column)}"
       else
         column = column.to_s
       end
       ids = ids.reverse if null_first
-      return relation.joins("LEFT JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{to_sql_values(ids)})) AS id(val)) AS id ON (#{column} = id.val)")
+      id_val = column_type == :uuid ? '(id.val)::uuid' : 'id.val'
+      values = to_sql_values(ids)
+      return relation.joins("LEFT JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{values})) AS id(val)) AS id ON (#{column} = #{id_val})")
                      .order(null_first ? 'row_number DESC' : 'row_number')
     end
 
@@ -30,7 +36,7 @@ module FindWithOrder::PGSupport
         return ids.join('),(')
         # return relation.order("array_position(ARRAY[#{ids.join(',')}], #{column})") #array_position is only support in PG >= 9.5
       when String
-        ids.map!{|s| ActiveRecord::Base.connection.quote_string(s) }
+        ids = ids.map{|s| ActiveRecord::Base.connection.quote_string(s) }
         return "'#{ids.join("'),('")}'"
         # return relation.order("array_position(ARRAY['#{ids.join("','")}']::varchar[], #{column})") #array_position is only support in PG >= 9.5
       else
--- test/order_uuid_test.rb
+++ test/order_uuid_test.rb
@@ -19,4 +19,10 @@ class OrderUuidTest < Minitest::Test
     test_order.call [@user3.id, @user1.id, @user2.id]
     test_order.call [@user3.id, @user2.id, @user1.id]
   end
+
+  def test_with_order
+    id_order = [@user3.id, @user1.id]
+    assert_equal [@user3.id, @user1.id, @user2.id], UuidUser.with_order(:id, id_order).pluck(:id)
+    assert_equal [@user2.id, @user3.id, @user1.id], UuidUser.with_order(:id, id_order, null_first: true).pluck(:id)
+  end
 end
