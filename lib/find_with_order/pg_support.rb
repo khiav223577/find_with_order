9be90c06705905beac2290a6b660e7784031e43c
Rumble Huang <mrtmrt15xn@yahoo.com.tw>
2019-05-12 12:46:05 +0800

    Fix &. is not supported in Ruby 2.2

--- lib/find_with_order/pg_support.rb
+++ lib/find_with_order/pg_support.rb
@@ -4,8 +4,7 @@ module FindWithOrder::PGSupport
   class << self
     def find_with_order(relation, ids)
       # return relation.where(id: ids).order("array_position(ARRAY[#{ids.join(',')}], #{relation.table_name}.id)").to_a #array_position is only support in PG >= 9.5
-      column_type = relation.columns_hash['id']&.type
-      id_val = column_type == :uuid ? '(id.val)::uuid' : 'id.val'
+      id_val = uuid?(relation, 'id') ? '(id.val)::uuid' : 'id.val'
       values = to_sql_values(ids)
       return relation.where(id: ids)
                      .joins("JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{values})) AS id(val)) AS id ON (#{relation.table_name}.id = #{id_val})")
@@ -17,14 +16,13 @@ module FindWithOrder::PGSupport
     end
 
     def with_order(relation, column, ids, null_first: false)
-      column_type = relation.columns_hash[column.to_s]&.type
+      id_val = uuid?(relation, column) ? '(id.val)::uuid' : 'id.val'
       if column.is_a?(Symbol) and relation.column_names.include?(column.to_s)
         column = "#{relation.connection.quote_table_name(relation.table_name)}.#{relation.connection.quote_column_name(column)}"
       else
         column = column.to_s
       end
       ids = ids.reverse if null_first
-      id_val = column_type == :uuid ? '(id.val)::uuid' : 'id.val'
       values = to_sql_values(ids)
       return relation.joins("LEFT JOIN (SELECT id.val, row_number() over() FROM (VALUES(#{values})) AS id(val)) AS id ON (#{column} = #{id_val})")
                      .order(null_first ? 'row_number DESC' : 'row_number')
@@ -44,6 +42,12 @@ module FindWithOrder::PGSupport
       else
         raise "not support type: #{ids.first.class}"
       end
+    end
+
+    def uuid?(relation, column)
+      column_info = relation.columns_hash[column.to_s]
+      return false if column_info == nil
+      return column_info.type == :uuid
     end
   end
 end
