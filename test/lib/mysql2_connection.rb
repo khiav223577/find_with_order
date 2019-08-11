1cc34eb41e97c1b374e8f4f5aefadc2467091ed7
khiav reoy <mrtmrt15xn@yahoo.com.tw>
2019-08-12 04:23:44 +0800

    Fix: create table fail
    Error:
    Mysql2::Error: All parts of a PRIMARY KEY must be NOT NULL; if you need NULL in a key, use UNIQUE instead: CREATE TABLE `users` (`id` int(11) DEFAULT NULL auto_increment PRIMARY KEY, `name` varchar(255), `email` varchar(255)) ENGINE=InnoDB (ActiveRecord::StatementInvalid)
    
    Code:
    https://stackoverflow.com/questions/40758226/mysql2error-all-parts-of-a-primary-key-must-be-not-null-if-you-need-null-in

--- test/lib/mysql2_connection.rb
+++ test/lib/mysql2_connection.rb
@@ -1,3 +1,9 @@
+require 'active_record/connection_adapters/mysql2_adapter'
+
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+end
+
 ActiveRecord::Base.establish_connection(
   "adapter"  => "mysql2",
   "database" => "travis_ci_test",
